dfcc306ebe483ac4015e5911561a2f5d99b0e472
SIRSoares33 <sirgustavo4@gmail.com>
2024-01-30 19:21:43 -0300

    feat: Criando a MainPresenter

--- YoutubeChannel/MVP/Presenter/Main/MainPresenter.cs
+++ YoutubeChannel/MVP/Presenter/Main/MainPresenter.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using YoutubeChannel.MVP.Model.Main;
+using YoutubeChannel.MVP.View.Main;
+using YoutubeChannel.Services;
 
-namespace YoutubeChannel.MVP.Presenter.Main
+namespace YoutubeChannel.MVP.Presenter.Main;
+
+public class MainPresenter
 {
-    internal class MainPresenter
+    #region Entities
+    private readonly IMainView _view;
+    private readonly IMainModel _model;
+    private readonly DbManager _database;
+    #endregion
+
+    #region Constructor
+    public MainPresenter(IMainView view, DbManager database,IMainModel model)
     {
+        _view = view;
+        _view.SearchButtonWasClicked += checkingSearch;
+
+        _database = database;
+        _database.CreateTablesIFNotExist();
+
+        _model = model;
+
+        getMyChannel();
     }
+    #endregion
+
+    #region Methods
+    private void checkingSearch() => throw new NotImplementedException();
+    #endregion
+
+    #region Initial Setup Methods
+    private void getMyChannel()
+    {
+        string[] MyChannelInfo = _model.GetingMyChannel(_database);
+
+        _view.MyChannelName += MyChannelInfo[0];
+        _view.MeusInscritos += MyChannelInfo[1];
+        _view.MyDate        += MyChannelInfo[2];
+    }
+    #endregion
 }
