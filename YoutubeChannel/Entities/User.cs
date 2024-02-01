96c8ad6505f1419bb238088b1668aaaa61988d99
SIRSoares33 <sirgustavo4@gmail.com>
2024-02-01 19:42:34 -0300

    Criando a entidade User

---
+++ YoutubeChannel/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace YoutubeChannel.Entities
+{
+    public class User
+    {
+        public required string Email    { get; set; }
+        public required string Password { get; set; }
+        public required string UserName { get; set; }
+    }
+}
